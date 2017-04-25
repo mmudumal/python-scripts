5dacecf2a77701046927a29a56bbe1099c14b90c
mmudumal <mmudumal>
2017-04-25 23:11:24 +0530

    Pleaseenter the commit message for your changes. Lines starting

--- meminfo/meminfoparameters.py
+++
@@ -1,9 +0,0 @@
-def parameters(memfile):
-    tlist=[]
-    mlist=[]
-    with open(memfile,'r') as sfile:
-        for line in sfile.readlines():
-            member=line.split()[0][0:-1]
-            tlist.append(member)
-        mlist=list(set(tlist))
-        return mlist
--- print-file.py
+++
@@ -1,9 +0,0 @@
-import os
-def print_file(filename):
-    with open(filename) as f:
-        for line in f.readlines().rstrip('\n'):
-            print(line)
-os.chdir('/home/mmudumal/python-scripts')
-print_file('pblogs_vmsodalqd022')
-        
-        
